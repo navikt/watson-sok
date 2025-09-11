72a2678148c859e7a2c18114bafa1ea288bd59a2
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-09-11 09:56:26 +0200

    Fjern base path fra env-fil, siden den ikke brukes

--- app/utils/env.ts
+++ app/utils/env.ts
@@ -1,5 +1,3 @@
-"use server";
-
 import { z } from "zod";
 
 const envSchema = z.object({
@@ -10,19 +8,18 @@ const envSchema = z.object({
   NAV_PERSONDATA_API_CLUSTER: z
     .string()
     .describe("The cluster name of the backend API"),
-  BASE_PATH: z.string().describe("The base path of the app"),
 });
 
-const envResultat = envSchema.safeParse(process.env);
+const envResult = envSchema.safeParse(process.env);
 
-if (!envResultat.success) {
-  console.error("Missing or invalid environment variables", envResultat.error);
+if (!envResult.success) {
+  console.error("Missing or invalid environment variables", envResult.error);
   throw new Error(
     "Invalid environment variables. Check console for more information."
   );
 }
 
-export const env = envResultat.data;
+export const env = envResult.data;
 export const isProd = env.NODE_ENV === "production";
 export const isTest = env.NODE_ENV === "test";
 export const isDev = env.NODE_ENV === "development";
--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ import { isProd } from "./app/utils/env";
 const nextConfig: NextConfig = {
   output: "standalone",
   assetPrefix: isProd ? "https://cdn.nav.no/holmes" : undefined,
-  /* config options here */
 };
 
 export default nextConfig;
