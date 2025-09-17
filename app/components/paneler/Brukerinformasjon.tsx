4e2fc37a5b34a4fa3490f8379f0d234cec2e93bb
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-09-17 16:00:51 +0200

    Refaktorer ut en felles panelcontainer

--- app/components/paneler/Brukerinformasjon.tsx
+++ app/components/paneler/Brukerinformasjon.tsx
@@ -1,4 +1,4 @@
-import { CopyButton, Heading, Link } from "@navikt/ds-react";
+import { CopyButton } from "@navikt/ds-react";
 import type { PersonInformasjon } from "~/routes/oppslag/[ident]/schemas";
 import { formatterAdresse } from "~/utils/adresse-utils";
 import { tilFulltNavn } from "~/utils/navn-utils";
@@ -6,6 +6,7 @@ import {
   formatterFødselsnummer,
   storFørsteBokstav,
 } from "~/utils/string-utils";
+import { PanelContainer } from "./PanelContainer";
 
 type BrukerinformasjonProps = {
   personInformasjon: PersonInformasjon;
@@ -24,13 +25,10 @@ export function Brukerinformasjon({
   ).length;
 
   return (
-    <div className="bg-primary rounded-sm border-1 border-gray-200 p-4 h-fit relative">
-      <Heading level="2" size="medium" spacing>
-        Brukerinformasjon
-      </Heading>
-      <div className="md:absolute top-4 right-4 mb-4">
-        <Link href="https://modia.nav.no">Historikk</Link>
-      </div>
+    <PanelContainer
+      title="Brukerinformasjon"
+      link={{ href: "https://modia.nav.no", beskrivelse: "Historikk" }}
+    >
       <dl className="grid sm:grid-cols-1 md:grid-cols-[1fr_2fr] gap-x-4 gap-y-2 [&>dt]:font-bold [&>dd]:flex [&>dd]:items-center [&>dd]:min-h-7">
         <dt>Navn</dt>
         <dd>
@@ -65,7 +63,7 @@ export function Brukerinformasjon({
           {antallFamilemedlemmer === 1 ? "familiemedlem" : "familemedlemmer"}
         </dd>
       </dl>
-    </div>
+    </PanelContainer>
   );
 }
 
