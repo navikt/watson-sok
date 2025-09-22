e94c94e7089f678c1eafc4aac2fc00410abbd2ec
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-09-22 08:44:30 +0200

    Gjør footeren mørk

--- app/components/footer/AppFooter.tsx
+++ app/components/footer/AppFooter.tsx
@@ -1,12 +1,18 @@
-import { BodyLong, Box } from "@navikt/ds-react";
+import { BodyLong, Box, Link as NavLink } from "@navikt/ds-react";
 import { Link } from "react-router";
 import { RouteConfig } from "~/config/routeConfig";
 
 export function AppFooter() {
   return (
-    <Box background="surface-subtle" padding="4" as="footer">
+    <Box background="surface-inverted" padding="4" as="footer">
       <BodyLong align="center">
-        <Link to={RouteConfig.PERSONVERN}>Personvern</Link>
+        <NavLink
+          as={Link}
+          to={RouteConfig.PERSONVERN}
+          style={{ color: "var(--a-text-on-inverted)" }}
+        >
+          Personvern
+        </NavLink>
       </BodyLong>
     </Box>
   );
