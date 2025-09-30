6967a1dc651949362f701d32adc861dd4a19038e
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-09-30 08:59:24 +0200

    Sjekk at brukeren eksisterer

--- app/routes/oppslag/api.server.ts
+++ app/routes/oppslag/api.server.ts
@@ -10,6 +10,44 @@ import {
   type OppslagBrukerRespons,
 } from "./schemas";
 
+type EksistensOgTilgangResponse = "ok" | "forbidden" | "not found" | "error";
+
+/** Sjekker eksistens og tilgang til en gitt personident */
+export async function sjekkEksistensOgTilgang(
+  ident: string,
+  request: Request,
+): Promise<EksistensOgTilgangResponse> {
+  const oboToken = await getnavpersondataapiOboToken(request);
+
+  try {
+    const response = await fetch(
+      "http://nav-persondata-api/oppslag/eksistens-og-tilgang",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${oboToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ident }),
+      },
+    );
+
+    switch (response.status) {
+      case 200:
+        return "ok";
+      case 403:
+        return "forbidden";
+      case 404:
+        return "not found";
+      default:
+        return "error";
+    }
+  } catch (error) {
+    console.error("⛔ Nettverksfeil mot baksystem:", error);
+    return "error";
+  }
+}
+
 /** Henter personopplysninger for en gitt ident */
 export async function hentPersonopplysninger(ident: string, request: Request) {
   return gjørOppslagApiRequest(ident, request, {
--- app/routes/oppslag/index.tsx
+++ app/routes/oppslag/index.tsx
@@ -1,8 +1,9 @@
-import { Heading, HGrid, Skeleton } from "@navikt/ds-react";
+import { Alert, Button, Heading, HGrid, Skeleton } from "@navikt/ds-react";
 import { use } from "react";
 import {
   redirect,
   useLoaderData,
+  useNavigate,
   type LoaderFunctionArgs,
   type MetaArgs,
 } from "react-router";
@@ -31,48 +32,71 @@ import {
   hentInntekter,
   hentPersonopplysninger,
   hentStønader,
+  sjekkEksistensOgTilgang,
 } from "./api.server";
 import type { PersonInformasjon } from "./schemas";
 
 export default function OppslagBruker() {
   const data = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
 
   return (
     <div className="flex flex-col gap-4 px-4">
-      <div className="mt-8 mb-4">
-        <ResolvingComponent
-          loadingFallback={
-            <Heading level="1" size="large" as={Skeleton}>
-              Navn Navnesen (xx)
-            </Heading>
-          }
-          errorFallback={
-            <Heading level="1" size="large">
-              Feil ved henting av bruker
-            </Heading>
-          }
-        >
-          <OverskriftPanel promise={data.personopplysninger} />
-        </ResolvingComponent>
-      </div>
-      <HGrid gap="space-24" columns={{ xs: 1, sm: 2, md: 2 }}>
-        <ResolvingComponent
-          loadingFallback={<BrukerinformasjonPanelSkeleton />}
-        >
-          <BrukerinformasjonPanel promise={data.personopplysninger} />
-        </ResolvingComponent>
-        <ResolvingComponent loadingFallback={<ArbeidsforholdPanelSkeleton />}>
-          <ArbeidsforholdPanel promise={data.arbeidsgiverInformasjon} />
-        </ResolvingComponent>
-      </HGrid>
-      <ResolvingComponent loadingFallback={<StønaderPanelSkeleton />}>
-        <StønaderPanel promise={data.stønader} />
-      </ResolvingComponent>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <ResolvingComponent loadingFallback={<InntektPanelSkeleton />}>
-          <InntektPanel promise={data.inntektInformasjon} />
-        </ResolvingComponent>
-      </div>
+      {data.eksistensOgTilgang === "forbidden" ? (
+        <Alert variant="warning" className="w-fit">
+          Du har ikke tilgang til å se denne personen
+        </Alert>
+      ) : data.eksistensOgTilgang === "not found" ? (
+        <Alert variant="warning" className="w-fit">
+          Ingen treff på fødsels- eller D-nummer
+        </Alert>
+      ) : data.eksistensOgTilgang === "error" ? (
+        <Alert variant="warning" className="w-fit">
+          En feil oppstod ved henting av bruker
+          <Button onClick={() => navigate(RouteConfig.INDEX)}>
+            Gå til forsiden
+          </Button>
+        </Alert>
+      ) : data.eksistensOgTilgang === "ok" ? (
+        <>
+          <div className="mt-8 mb-4">
+            <ResolvingComponent
+              loadingFallback={
+                <Heading level="1" size="large" as={Skeleton}>
+                  Navn Navnesen (xx)
+                </Heading>
+              }
+              errorFallback={
+                <Heading level="1" size="large">
+                  Feil ved henting av bruker
+                </Heading>
+              }
+            >
+              <OverskriftPanel promise={data.personopplysninger} />
+            </ResolvingComponent>
+          </div>
+          <HGrid gap="space-24" columns={{ xs: 1, sm: 2, md: 2 }}>
+            <ResolvingComponent
+              loadingFallback={<BrukerinformasjonPanelSkeleton />}
+            >
+              <BrukerinformasjonPanel promise={data.personopplysninger} />
+            </ResolvingComponent>
+            <ResolvingComponent
+              loadingFallback={<ArbeidsforholdPanelSkeleton />}
+            >
+              <ArbeidsforholdPanel promise={data.arbeidsgiverInformasjon} />
+            </ResolvingComponent>
+          </HGrid>
+          <ResolvingComponent loadingFallback={<StønaderPanelSkeleton />}>
+            <StønaderPanel promise={data.stønader} />
+          </ResolvingComponent>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <ResolvingComponent loadingFallback={<InntektPanelSkeleton />}>
+              <InntektPanel promise={data.inntektInformasjon} />
+            </ResolvingComponent>
+          </div>
+        </>
+      ) : null}
     </div>
   );
 }
@@ -83,10 +107,10 @@ export async function loader({ request }: LoaderFunctionArgs) {
     return redirect(RouteConfig.INDEX);
   }
 
-  // TODO: Sjekk om personen finnes / at man har tilgang til å se dem
-  // gjennom et eget endepunkt, før resten returneres
+  const eksistensOgTilgang = await sjekkEksistensOgTilgang(ident, request);
 
   return {
+    eksistensOgTilgang,
     personopplysninger: hentPersonopplysninger(ident, request),
     arbeidsgiverInformasjon: hentArbeidsgivere(ident, request),
     inntektInformasjon: hentInntekter(ident, request),
