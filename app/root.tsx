d3821668a3f9de48112c9b890b42c70c304de766
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-10-23 15:48:36 +0200

    Refaktorer mixpanel og legg til i csp

--- app/root.tsx
+++ app/root.tsx
@@ -22,7 +22,7 @@ import {
 } from "./features/darkside/ThemeCookie";
 import { InternalServerError } from "./features/feilhåndtering/InternalServerError";
 import { PageNotFound } from "./features/feilhåndtering/PageNotFound";
-import { AnalyticsTag } from "./utils/analytics";
+import { AnalyticsTags } from "./utils/analytics";
 import { initFaro } from "./utils/observability";
 
 export default function Root() {
@@ -65,7 +65,7 @@ export function headers() {
       "geolocation=(), microphone=(), camera=(), payment=(), usb=(), magnetometer=(), gyroscope=(), accelerometer=()",
     "Content-Security-Policy":
       "default-src 'self'; " +
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
+      "script-src 'self' https://cdn.nav.no https://cdn.mxpnl.com 'unsafe-inline' 'unsafe-eval'; " +
       "style-src 'self' 'unsafe-inline'; " +
       "img-src 'self' data: https:; " +
       "font-src 'self' data: cdn.nav.no; " +
@@ -110,21 +110,7 @@ function HtmlRamme({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
-        {umamiSiteId && <AnalyticsTag sporingId={umamiSiteId} />}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-      (function(e,c){if(!c.__SV){var l,h;window.mixpanel=c;c._i=[];c.init=function(q,r,f){function t(d,a){var g=a.split(".");2==g.length&&(d=d[g[0]],a=g[1]);d[a]=function(){d.push([a].concat(Array.prototype.slice.call(arguments,0)))}}var b=c;"undefined"!==typeof f?b=c[f]=[]:f="mixpanel";b.people=b.people||[];b.toString=function(d){var a="mixpanel";"mixpanel"!==f&&(a+="."+f);d||(a+=" (stub)");return a};b.people.toString=function(){return b.toString(1)+".people (stub)"};l="disable time_event track track_pageview track_links track_forms track_with_groups add_group set_group remove_group register register_once alias unregister identify name_tag set_config reset opt_in_tracking opt_out_tracking has_opted_in_tracking has_opted_out_tracking clear_opt_in_out_tracking start_batch_senders start_session_recording stop_session_recording people.set people.set_once people.unset people.increment people.append people.union people.track_charge people.clear_charges people.delete_user people.remove".split(" ");
-      for(h=0;h<l.length;h++)t(b,l[h]);var n="set set_once union unset remove delete".split(" ");b.get_group=function(){function d(p){a[p]=function(){b.push([g,[p].concat(Array.prototype.slice.call(arguments,0))])}}for(var a={},g=["get_group"].concat(Array.prototype.slice.call(arguments,0)),m=0;m<n.length;m++)d(n[m]);return a};c._i.push([q,r,f])};c.__SV=1.2;var k=e.createElement("script");k.type="text/javascript";k.async=!0;k.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"file:"===
-      e.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\\/\\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";e=e.getElementsByTagName("script")[0];e.parentNode.insertBefore(k,e)}})(document,window.mixpanel||[]);
-      mixpanel.init('f5e4c5b5414a87e94d8d4182e4c458c2', {
-        autocapture: true,
-        record_sessions_percent: 100,
-        api_host: 'https://api-eu.mixpanel.com'
-      });
-    `,
-          }}
-        />
+        {umamiSiteId && <AnalyticsTags sporingId={umamiSiteId} />}
       </head>
       <body className="flex flex-col min-h-screen">
         <FaroErrorBoundary>
