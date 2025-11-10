303391f5e5406c5fa276d1b1f4a0d687c6915bd9
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-11-10 23:41:58 +0100

    Prøv å sett det til null

--- app/routes/oppslag/schemas.ts
+++ app/routes/oppslag/schemas.ts
@@ -74,7 +74,7 @@ export const PersonInformasjonSchema = z.object({
   sivilstand: z.string().nullable(),
   alder: z.number(),
   fødselsdato: z.string(),
-  dødsdato: z.string().nullable().optional(),
+  dødsdato: z.string().or(z.null()),
 });
 
 export type PersonInformasjon = z.infer<typeof PersonInformasjonSchema>;
