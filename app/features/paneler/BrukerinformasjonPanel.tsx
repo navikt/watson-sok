a055d47112992428004057512d4c5e7cfc9cc719
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-11-19 09:47:58 +0100

    Gjør personinfo fullbredde

--- app/features/paneler/BrukerinformasjonPanel.tsx
+++ app/features/paneler/BrukerinformasjonPanel.tsx
@@ -64,7 +64,7 @@ const BrukerinformasjonPanelMedData = ({
         beskrivelse: "Historikk",
       }}
     >
-      <dl className="grid sm:grid-cols-1 md:grid-cols-[1fr_2fr] gap-x-4 gap-y-2 [&>dt]:font-bold [&>dd]:flex [&>dd]:items-center [&>dd]:min-h-7">
+      <dl className="grid grid-cols-1 md:grid-cols-[1fr_2fr_1fr_2fr] 2xl:grid-cols-[1fr_2fr_1fr_2fr_1fr_2fr] gap-x-4 gap-y-2 [&>dt]:font-bold [&>dd]:flex [&>dd]:items-center [&>dd]:min-h-7">
         <dt>Navn</dt>
         <dd>
           {tilFulltNavn(personopplysninger.navn)}{" "}
