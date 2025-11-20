fb4d7d9cebcfe83c077f4c27e7804ed123b26bd8
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-11-20 10:56:05 +0100

    Legg til en whitelist for hvem som får lov til å få kalle logged-in-user endepunktet

--- app/routes/api/logged-in-user/index.ts
+++ app/routes/api/logged-in-user/index.ts
@@ -1,6 +1,19 @@
 import type { LoaderFunctionArgs } from "react-router";
 import { getLoggedInUser } from "~/utils/access-token";
 
+const brukereSomHarTilgang = {
+  hansJacob: "M118976",
+  kristofer: "S162301",
+  snorri: "E176931",
+  sturle: "H139079",
+  // Testbruker vi bruker i dev
+  devTestbruker: "Z994531",
+};
+
 export async function loader({ request }: LoaderFunctionArgs) {
-  return getLoggedInUser({ request });
+  const loggedInUser = await getLoggedInUser({ request });
+  if (Object.values(brukereSomHarTilgang).includes(loggedInUser.navIdent)) {
+    return loggedInUser;
+  }
+  return Response.json({ error: "Du har ikke tilgang" }, { status: 403 });
 }
