4eae2546aecd257d86d80ea6c5b43ec4bf86846c
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-12-03 09:54:49 +0100

    Lag en context for tidsvindu

---
+++ app/routes/oppslag/Tidsvindu.tsx
@@ -0,0 +1,68 @@
+import { ToggleGroup } from "@navikt/ds-react";
+import { ToggleGroupItem } from "@navikt/ds-react/ToggleGroup";
+import { createContext, use, useState } from "react";
+
+type TidsvinduPeriode = "6 måneder" | "1 år" | "3 år";
+
+type TidsvinduContextType = {
+  tidsvindu: TidsvinduPeriode;
+  setTidsvindu: (tidsvindu: TidsvinduPeriode) => void;
+};
+const TidsvinduContext = createContext<TidsvinduContextType | null>(null);
+
+/**
+ * Holder styr på hvilket tidsvindu som skal vises i visualiseringer
+ */
+export const TidsvinduProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [tidsvindu, setTidsvindu] = useState<TidsvinduPeriode>("3 år");
+  return (
+    <TidsvinduContext.Provider value={{ tidsvindu, setTidsvindu }}>
+      {children}
+    </TidsvinduContext.Provider>
+  );
+};
+
+/**
+ * Returnerer tidsvindu og en måte å endre det på
+ */
+export const useTidsvindu = () => {
+  const context = use(TidsvinduContext);
+  if (!context) {
+    throw new Error("useTidsvindu må brukes innenfor en TidsvinduProvider");
+  }
+  return {
+    ...context,
+    tidsvinduIAntallMåneder:
+      context.tidsvindu === "6 måneder"
+        ? 6
+        : context.tidsvindu === "1 år"
+          ? 12
+          : 36,
+  };
+};
+
+/**
+ * Lar brukeren velge hvor stort tidsvindu som skal vises i visualiseringer
+ */
+export const TidsvinduVelger = () => {
+  const { tidsvindu, setTidsvindu } = useTidsvindu();
+  return (
+    <div className="sticky p-4 top-18 right-4 z-10 bg-ax-surface-primary">
+      <ToggleGroup
+        variant="neutral"
+        size="small"
+        value={tidsvindu}
+        aria-label="Velg tidsvindu"
+        onChange={(value) => setTidsvindu(value as TidsvinduPeriode)}
+      >
+        <ToggleGroupItem value="6 måneder" label="6 mnd" />
+        <ToggleGroupItem value="1 år" label="1 år" />
+        <ToggleGroupItem value="3 år" label="3 år" />
+      </ToggleGroup>
+    </div>
+  );
+};
