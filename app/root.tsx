c31a3fe1ca89e272a3a00fd6087a207e87d3d81e
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-12-12 11:10:47 +0100

    Fiks en export bug

--- app/root.tsx
+++ app/root.tsx
@@ -2,9 +2,10 @@
  * Dette er entry-pointet til appen. Ta en titt på features/layout/root.tsx for mer informasjon.
  */
 
+import { rootLoader } from "~/layout/loader.server";
 import { default as RootRoute } from "~/layout/root";
 
 export default RootRoute;
 export { ErrorBoundary } from "~/layout/ErrorBoundary";
-export { rootLoader as loader } from "~/layout/loader.server";
 export { headers } from "~/sikkerhet/headers";
+export const loader = rootLoader;
