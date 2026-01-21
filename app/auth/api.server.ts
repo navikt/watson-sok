afd070dcdb71182d48a1ffe2364ad1c8b25d80a0
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2026-01-21 15:09:38 +0100

    Legg til feilhåndtering

--- app/auth/api.server.ts
+++ app/auth/api.server.ts
@@ -5,22 +5,33 @@ import { SaksbehandlerInfoSchema, type SaksbehandlerInfo } from "./domene";
 export async function hentSaksbehandlerInfo(
   token: string,
 ): Promise<SaksbehandlerInfo> {
-  const response = await fetch(`${BACKEND_API_URL}/meg`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  const json = await response.json();
-  const parsedData = SaksbehandlerInfoSchema.safeParse(json);
-  if (!parsedData.success) {
-    logger.warn("Kunne ikke hente informasjon om saksbehandler", {
-      feilmelding: json.error,
-      status: response.status,
+  try {
+
+
+    const response = await fetch(`${BACKEND_API_URL}/meg`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     });
+    const json = await response.json();
+    const parsedData = SaksbehandlerInfoSchema.safeParse(json);
+    if (!parsedData.success) {
+      logger.warn("Informasjon fra saksbehandler-API var ugyldig", {
+        feilmelding: json.error,
+        status: response.status,
+      });
+      return {
+        navIdent: "",
+        organisasjoner: ["Ukjent"],
+      };
+    }
+    return parsedData.data;
+
+  } catch (error) {
+    logger.error("Kunne ikke hente informasjon om saksbehandler", { error });
     return {
       navIdent: "",
       organisasjoner: ["Ukjent"],
     };
   }
-  return parsedData.data;
 }
