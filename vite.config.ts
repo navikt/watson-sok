7a5d17e5d5c4b5b6af583d5b17c7df84e0763735
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2026-02-11 16:30:27 +0100

    Fjern react-router-devtools
    Den førte bare til trøbbel

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { reactRouterDevTools } from "react-router-devtools";
 import { defineConfig } from "vite";
 import devtoolsJson from "vite-plugin-devtools-json";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -9,11 +8,10 @@ export default defineConfig({
   base: "/",
   plugins: [
     tailwindcss(),
-    process.env.NODE_ENV === "development" ? reactRouterDevTools() : null,
     reactRouter(),
     process.env.NODE_ENV === "development" ? devtoolsJson() : null,
     tsconfigPaths(),
-  ].filter(Boolean), // Fjern dev only plugins
+  ].filter(Boolean),
   server: {
     hmr: {
       path: "/",
